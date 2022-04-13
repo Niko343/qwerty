7d63a7df6ed65a1140363b911df0a9d24aec430c
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-14 00:34:59 +0300

    МАША

---
+++ Ex005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
