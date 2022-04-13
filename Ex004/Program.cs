9c3212f4c0f1ae2009f5901ed89082b8e41eac0c
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-14 00:18:23 +0300

    Random

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,1 +1,6 @@
-﻿int numberA = Console.ReadLine();
+﻿int numberA = new Random().Next(1, 20);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 20);
+Console.WriteLine(numberB);
+double res = numberA + numberB;
+Console.WriteLine(res);
